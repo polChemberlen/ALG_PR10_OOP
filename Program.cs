2670349186810f98579f4f6541e7ab319699b746
polChemberlen <0109333fd@gmail.com>
2025-12-02 23:08:09 +0500

    n2

--- Program.cs
+++ Program.cs
@@ -1,130 +1,118 @@
-﻿/*Практическая работа № 10
-
-Использование принципов ООП
-
-Задание: 
-1.Написать код, показывающий работу 4-ех принципов ООП
-2. В решение добавить проект, реализовать перегрузку методов, операторов и преобразование типов
-3. Реализовать программу, представленную на блок-схеме (отступать от действий, показанных на ней, нельзя)*/
-
+﻿/*2.В решение добавить проект, 
+ * реализовать перегрузку методов, 
+ * операторов и преобразование типов*/
 
 using System;
+using System.Threading.Channels;
 
-//Задание 1
-namespace OOP
+namespace OverLoading
 {
-    //Инкапсуляция
-    class Camarero
+    //Перегрузка операторов
+    struct Point
     {
-        private bool estaListo;
+        public int x, y;
 
-        private void Preparar()
+        public Point(int x, int y)
         {
-            Console.WriteLine("Блюдо готовиться");
-
-            estaListo = true;
-
-            Console.WriteLine("Блюдо готово");
+            this.x = x;
+            this.y = y;
         }
 
-        public void Servicio()
+        public static Point operator +(Point a, Point b)
         {
-            if (!estaListo)
-            {
-                Console.WriteLine("Блюдо не готово");
-                Preparar();
-            }
-            Console.WriteLine("Ваше жаренные гвозди, сеньор \n");
-            estaListo = false;
-        }
-
-    }
-
-    //Наследование и полиморфизм
-    class Pizza
-    {
-        public string Ingredientes { get; set; }
-        public string CookingTime { get; set; }
-
-        public void ToCook()
-        {
-            Console.WriteLine($"У нас есть пицца с {Ingredientes}. Она будет готова примерно через {CookingTime}");
-        }
-
-        public void Cooking()
-        {
-            Console.WriteLine("Ваша пицца готовиться \n");
+            return new Point(a.x + b.x, a.y + b.y);
         }
     }
-
-    class Margarita : Pizza
-    {
-        public void Order()
-        {
-            Console.WriteLine("Вы выбрали пиццу Маргарита \n");
-        }
-
-        public void Cooking()
-        {
-            Console.WriteLine("Ваша Маргарита готовиться \n");
-
-        }
-    }
-
-    //Абстракция
-    abstract class CookingPizza
-    {
-        public abstract void Cook();
-    }
-
-    class CookingMargarita : CookingPizza
-    {
-        public override void Cook()
-        {
-            Console.WriteLine("Начинка для Маргариты выкладывается на тесто");
-        }
-    }
-
-    class Pizzayolo
-    {
-        public void Cook(CookingPizza cookingPizza)
-        {
-            cookingPizza.Cook();
-        }
-    }
-
-
-
-
-
     class Programm
     {
+        //Перегрузка методов
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        //преобразование типов
+
+        public class Kg
+        {
+            public double Value { get; set; }
+
+            public Kg(double value)
+            {
+                Value = value;
+            }
+
+            //Неяновное преобразование 
+            public static implicit operator Gram(Kg kg)
+            {
+                return new Gram(kg.Value * 1000);
+            }
+
+            //Явное преобразование
+            public static explicit operator Kg(Gram gram)
+            {
+                return new Kg(gram.Value / 1000);
+            }
+
+            public override string ToString()
+            {
+                return $"{Value} кг";
+            }
+        }
+
+        public class Gram
+        {
+            public double Value { get; set; }
+
+            public Gram(double value)
+            {
+                Value = value;
+            }
+
+            public override string ToString()
+            {
+                return $"{Value} г";
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            //Инкапсуляция
-            Console.WriteLine("Инкапсуляция: ");
-            Camarero camarero = new Camarero();
-            camarero.Servicio();
 
-            //Наследование
-            Console.WriteLine("Наследование: ");
-            Margarita margarita = new Margarita { Ingredientes = "Томатный соус, сыр", CookingTime = "42 минуты" };
-            margarita.ToCook();
-            margarita.Order();
+            //Перегрузка методов
+            int result = Sum(1, 2, 5);
+            Console.WriteLine($"{result}\n");
 
-            //Полиморфизм
-            Console.WriteLine("Полиморфизм: ");
-            Pizza pizza = new Margarita { Ingredientes = "Томатный соус, сыр", CookingTime = "52 минуты" };
-            pizza.Cooking();
 
-            //Абстракция
-            Console.WriteLine("Абстракция: ");
-            Pizzayolo pizzayolo = new Pizzayolo();
-            CookingMargarita cookingMargarita = new CookingMargarita();
-            pizzayolo.Cook(cookingMargarita);
+            //Перегрузка операторов
+            Point a = new Point(42, 52);
+            Point b = new Point(9, 11);
+            Point c = a + b;
+            Console.WriteLine("Точка c: x = {0}, y = {1}", c.x, c.y);
+
+
+
+            Kg kg = new Kg(1.5);
+            //Неявное 
+            Gram g = kg;
+            Console.WriteLine(g);
+
+            Gram gram = new Gram(4200);
+
+            //Явное
+            Kg kgg = (Kg)gram;
+            Console.WriteLine(kgg);
         }
     }
+
 }
-
-
-
